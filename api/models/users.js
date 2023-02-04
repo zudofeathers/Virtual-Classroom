e477554a32a2cc88a7f75e58a2cac2be3f46a5bd
Adam el Hassan <46625870+zudofeathers@users.noreply.github.com>
2023-02-04 21:09:36 +0100

    rever ES6 for login because it didnt work

--- api/models/users.js
+++ api/models/users.js
@@ -32,21 +32,21 @@ var userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.methods.setPassword = (password) => {
+userSchema.methods.setPassword = function (password) {
   this.salt = crypto.randomBytes(16).toString("hex");
   this.hash = crypto
     .pbkdf2Sync(password, this.salt, 1000, 64, "sha512")
     .toString("hex");
 };
 
-userSchema.methods.validPassword = (password) => {
+userSchema.methods.validPassword = function (password) {
   var hash = crypto
     .pbkdf2Sync(password, this.salt, 1000, 64, "sha512")
     .toString("hex");
   return this.hash === hash;
 };
 
-userSchema.methods.generateJwt = () => {
+userSchema.methods.generateJwt = function () {
   var expiry = new Date();
   expiry.setDate(expiry.getDate() + 7);
 
