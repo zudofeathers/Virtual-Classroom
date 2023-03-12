6f139dc019c5b15a35ceb9215df02b31da28101c
Adam el Hassan <46625870+zudofeathers@users.noreply.github.com>
2023-03-13 00:54:21 +0100

    Fixed admin not seeing all courses

--- client/src/app/dashboard/dashboard.component.ts
+++ client/src/app/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { AuthenticationService, UserDetails } from "../authentication.service";
 import { Router } from "@angular/router";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Course } from "../models/course";
 
 @Component({
   /*selector: 'app-dashboard',*/
@@ -10,7 +11,7 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
 })
 export class DashboardComponent {
   user: UserDetails;
-  courses: Object[] = [];
+  courses: Course[] = [];
   newcourse = {
     code: "",
     name: "",
@@ -19,7 +20,6 @@ export class DashboardComponent {
     assignmentDeadline: null,
   };
   find = false;
-  list: Object = [];
 
   constructor(
     private auth: AuthenticationService,
@@ -33,15 +33,7 @@ export class DashboardComponent {
     this.auth.profile().subscribe(
       (user) => {
         this.user = user;
-        //Log the user details
-        //Load MyCourses
-        for (var course in user.courses) {
-          this.http
-            .get("/api/courseDetails/" + user.courses[course], this.httpOptions)
-            .subscribe((res) => {
-              this.courses.push(res);
-            });
-        }
+        this.allCourses();
         this.newcourse.owner = user._id;
       },
       (err) => {
@@ -73,10 +65,12 @@ export class DashboardComponent {
       });
   }
   allCourses() {
-    this.http.get("/api/allCourses/", this.httpOptions).subscribe((res) => {
-      this.list = res;
-      this.find = true;
-    });
+    this.http
+      .get("/api/allCourses/", this.httpOptions)
+      .subscribe((res: Course[]) => {
+        this.courses = res;
+        this.find = true;
+      });
   }
   joinCourse(courseCode: string) {
     if (!this.user.faculty) {
